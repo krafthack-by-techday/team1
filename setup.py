5932a283aa59b88f707de4d89c09d458b0d6b85f
bzapf <bastian.zapf@hafslundeco.no>
2025-04-24 12:43:31 +0200

    remove comment

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import find_packages, setup
 setup(
     name="norgespriskalkulator",
     version="0.1",
-    packages=find_packages(),  # <-- finds app and app.submodule
+    packages=find_packages(),
 )
