a1b78e52a99572230e52d80400a48bd3c1979bff
bzapf <bastian.zapf@hafslundeco.no>
2025-04-24 14:50:15 +0200

    initial draft for frontend

---
+++ frontend/pages/Disclaimer.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Disclaimer",
+    page_icon="‼️",
+)
+
+st.write("# Disclaimer ")
+
+
+st.markdown(
+    """
+    IKKE LAD TESLAEN DIN KL 16 !!!"
+    """
+)
---
+++ frontend/pages/Learn.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Learn",
+    page_icon="🎓",
+)
+
+st.write("# Hello! ")
+
+
+st.markdown(
+    """
+    What do you want want to learn about?
+    """
+)
