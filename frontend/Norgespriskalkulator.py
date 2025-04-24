8ba448a18ebf1b91acc2a9f2542a237b5d351ad3
bzapf <bastian.zapf@hafslundeco.no>
2025-04-24 16:04:05 +0200

    fix

--- frontend/Norgespriskalkulator.py
+++ frontend/Norgespriskalkulator.py
@@ -55,7 +55,8 @@ config = controls()
 
 
 def get_price_area(metering_point_id: str) -> str | None:
-    if metering_point_id != "Din Målepunkt ID":
+    if metering_point_id != "":  # The default return value of the text widget -
+        # until the user has entered their ID, we do not know the price area
         return "NO42"
     return None
 
