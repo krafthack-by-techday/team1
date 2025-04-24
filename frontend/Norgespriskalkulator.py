1e289db5f1b82cf4615bdb1bd4a46ee3584cfcc3
Bastian Zapf <166711260+bzapf-heco@users.noreply.github.com>
2025-04-24 17:05:37 +0200

    update (#27)

--- frontend/Norgespriskalkulator.py
+++ frontend/Norgespriskalkulator.py
@@ -4,16 +4,26 @@ import streamlit as st
 from dotenv import load_dotenv
 from PIL import Image
 
-from frontend.graphics.controls import controls
-from frontend.graphics.elements import colored_box
-from frontend.graphics.make_plot import make_plot
-from frontend.utils.utils import get_price_area
+try:
+    from frontend.graphics.controls import controls
+    from frontend.graphics.elements import colored_box
+    from frontend.graphics.make_plot import make_plot
+    from frontend.utils.utils import get_price_area
+except ModuleNotFoundError:
+    msg = "Could not import code, did you run pip install -e . ?"
+    raise ModuleNotFoundError(msg)
 
 load_dotenv("CONFIG.env")
 logo = Image.open("assets/logo-green.png")
 icon = Image.open("assets/ikon-gold.png")
 
+
 st.set_page_config(page_title="Norgespriskalkulator", page_icon=icon, layout="wide")
+
+with st.sidebar:
+    st.image(logo, use_container_width=False, width=200)
+
+
 st.write("""### Hei 👋 Her kan du sjekke om Norgesprisen lønner seg for deg 💰""")
 
 norgespriscolor = os.environ["NORGESPRISCOLOR"]
@@ -57,5 +67,14 @@ if config.input_is_set is True:
         )
 
 
-with st.sidebar:
-    st.image(logo, use_container_width=True)
+@st.dialog("Informasjon om Norgespriskalkulator")
+def vote(item):
+    st.write(
+        "Ikke lad din Tesla kl 16."
+        "Hvis hele Norge lager Grandiossa 🍕 og lader Tesla 🏎️ til samme tid, så blir det strømutfall ⚡"
+    )
+
+
+if "vote" not in st.session_state:
+    if st.sidebar.button("Informasjon om Norgespriskalkulator"):
+        vote("Informasjon")
