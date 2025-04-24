3e10d34b2e9b2b85181dbde009127350b01b1167
bzapf <bastian.zapf@hafslundeco.no>
2025-04-24 12:49:57 +0200

    add test

--- tests/test_backend/test_adapter/test_price_fetcher/test_local_price_fetcher.py
+++ tests/test_backend/test_adapter/test_price_fetcher/test_local_price_fetcher.py
@@ -1,3 +1,5 @@
+import os
+import warnings
 from datetime import datetime
 from pathlib import Path
 from zoneinfo import ZoneInfo
@@ -21,3 +23,21 @@ class TestLocalSpotPriceFetcher:
             (datetime(2025, 4, 16, hour=20, tzinfo=ZoneInfo("UTC")), 12.34),
             (datetime(2025, 4, 16, hour=21, tzinfo=ZoneInfo("UTC")), 42.00),
         ]
+
+    def test_with_original_data(self) -> None:
+        try:
+            path = os.environ["PATH_TO_NORWAY_PRICES"]
+        except KeyError:
+            msg = "Environment variable PATH_TO_NORWAY_PRICES not set, skipping test"
+            warnings.warn(msg)
+            return
+
+        fetcher = LocalSpotPriceFetcher(path_to_norway_data=Path(path))
+
+        prices = fetcher.get_price(
+            price_area="NO1",
+            start=datetime(2025, 4, 16, hour=20, tzinfo=ZoneInfo("UTC")),
+            end=datetime(2025, 4, 16, hour=21, tzinfo=ZoneInfo("UTC")),
+        )
+
+        assert len(prices) == 2
